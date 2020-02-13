ad81fa9cfec745b82afec46f8b2e06f5647b4bd5
Nathan Chanteux <nathan@eteamsys.lu>
2020-02-13 14:09:56 +0100

    initial commit

---
+++ Interfaces/WpTraitInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace nlib\Wordpress\Interfaces;
+
+interface WpTraitInterface {
+    
+    /**
+     *
+     * @return string
+     */
+    public function get_language() : string;
+}
---
+++ Traits/WpTrait.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace nlib\Wordpress\Traits;
+
+trait WpTrait {
+
+    public function get_language() : string {
+        return function_exists('icl_object_id') && defined('ICL_LANGUAGE_CODE') ? ICL_LANGUAGE_CODE : get_bloginfo('language');
+    }
+}
