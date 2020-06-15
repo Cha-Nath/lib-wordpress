1ef04ab45227e982cef5b76dbdd212dd89737dea
_ <firminchanteux@gmail.com>
2020-06-15 12:12:09 +0200

    update

--- src/Traits/WpTrait.php
+++ src/Traits/WpTrait.php
@@ -5,6 +5,6 @@ namespace nlib\Wordpress\Traits;
 trait WpTrait {
 
     public function get_language() : string {
-        return function_exists('icl_object_id') && defined('ICL_LANGUAGE_CODE') ? ICL_LANGUAGE_CODE : get_bloginfo('language');
+        return function_exists('icl_object_id') && defined('ICL_LANGUAGE_CODE') ? ICL_LANGUAGE_CODE ?? '' : get_bloginfo('language');
     }
 }
