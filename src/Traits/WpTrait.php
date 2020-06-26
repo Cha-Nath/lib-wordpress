6befce0a7e37d3adfcc0bdbdefd1f11e63a29aa1
_ <firminchanteux@gmail.com>
2020-06-26 11:23:12 +0200

    update

--- src/Traits/WpTrait.php
+++ src/Traits/WpTrait.php
@@ -7,4 +7,8 @@ trait WpTrait {
     public function get_language() : string {
         return function_exists('icl_object_id') && defined('ICL_LANGUAGE_CODE') ? ICL_LANGUAGE_CODE ?? '' : get_bloginfo('language');
     }
+
+    public function get_current_user_email() : string {
+        return !empty(($User = wp_get_current_user())->data) ? $User->data->user_email : '';
+    }
 }
